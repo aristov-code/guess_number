242354bcda108b810d4ac9c8cbb56e2494f74a6e
Ilya Aristov <aristow.ilja@yandex.ru>
2024-09-23 17:58:57 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -8,12 +8,12 @@ while True:
 
     if guess < number:
         print('Ваше число меньше того, что загадано')
-    if guess > number:
+    elif guess > number:
         # ...выводим сообщение.
         print('Ваше число больше того, что загадано.')
     
     # Если число угадано...
-    if guess == number:
+    elif guess == number:
         # ...прерываем выполнение программы и...
         break
 # ...выводим сообщение.
